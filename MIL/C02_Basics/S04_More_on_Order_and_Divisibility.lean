c0c030704c16fa3f612ac3c2e7cc447216f362e6
Youran Lu <youran.lu@zhichun.org>
2025-10-12 13:57:51 +0800

    update

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,78 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
+  have h : ∀ x y : ℝ, max x y ≤ max y x := by
+    intro x y
+    apply max_le
+    apply le_max_right
+    apply le_max_left
+  apply le_antisymm
+  repeat
+    apply h
+-- 和min a b = min b a一样的过程
+
 
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply le_antisymm
+  · apply le_min
+    · apply le_trans
+      · apply min_le_left
+      apply min_le_left
+    apply le_min
+    · apply le_trans
+      apply min_le_left
+      apply min_le_right
+  apply le_min
+  · apply le_min
+    apply le_trans
+    apply min_le_left
+    apply le_refl a
+  apply le_trans
+  apply min_le_right
+  apply min_le_right
+--不知道为什么报错
+
+
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  · apply add_le_add_right
+    apply min_le_left
+  apply add_le_add_right
+  apply min_le_right
+-- 差点想不到这个定理add_le_add_right
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  · apply aux
+-- apply le_trans
+-- · apply min_le_left
+-- apply add_le_add_right
+-- 这一步又让我证明a< min(a,b)然后卡住
+  have h: min (a +c) (b+c)= min (a+c) (b+c)-c+c := by
+    rw[sub_add_cancel]
+  rw[h]
+  apply add_le_add_right
+  apply le_min
+  · nth_rw 2 [← add_neg_cancel_right a c]
+    apply sub_le_sub_right
+    apply min_le_left
+  nth_rw 2 [← add_neg_cancel_right b c]
+  apply sub_le_sub_right
+  apply min_le_right
+-- 终于！试出这种写法
+-- 刚开始写的时候误用了min_add然后一行就成了？？
+-- 遂发现这是要证明的定理
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  nth_rw 1 [← sub_add_cancel a b]
+  have h:|a - b + b| - |b| ≤|a - b| + |b| - |b|:=by
+    apply sub_le_sub_right
+    apply abs_add
+  linarith
+-- 还在纠结定理的时候发现linarith竟然可以直接解决
 end
 
 section
@@ -66,7 +127,16 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  · apply dvd_add
+    · apply dvd_mul_of_dvd_right
+      apply dvd_mul_right
+    apply dvd_mul_left
+  rw[pow_two]
+  apply dvd_mul_of_dvd_left
+  exact h
+-- 这个有点容易出错，对dvd的定理还不是很熟
+
 end
 
 section
@@ -78,5 +148,12 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply Nat.dvd_antisymm
+  apply Nat.dvd_gcd
+  apply Nat.gcd_dvd_right
+  apply Nat.gcd_dvd_left
+  apply Nat.dvd_gcd
+  apply Nat.gcd_dvd_right
+  apply Nat.gcd_dvd_left
+-- 这边可以用repeat的
 end
